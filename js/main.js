21a2a59f064c64569ac11ebe58ab74f1699ebcd7
Umaima <98324267+Umaima-Fatima@users.noreply.github.com>
2024-08-04 15:09:54 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*===================== Typing Animation =====================*/
+var typed = new Typed(".typing", {
+    strings:["", "Mern Stack Developer", "UI Designer", "React JS Developer", "Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
